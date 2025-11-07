d78a73984c274e35541e08df8fe5f7ca60fc00f5
jotv <josefuccef7@gmail.com>
2025-11-07 17:18:24 +0100

    up

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function subtractOneHour(time) {
     return time; // إذا كانت القيمة غير صالحة، أعد الوقت الأصلي
   }
 
-  date.setHours(date.getHours() - 2); // إنقاص ساعة
+  date.setHours(date.getHours() - 1); // إنقاص ساعة
 
   const updatedHours = String(date.getHours()).padStart(2, '0');
   const updatedMinutes = String(date.getMinutes()).padStart(2, '0');
