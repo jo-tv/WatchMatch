3bf09c7c0cfe7707d7a9bf62142c56b0fc05eac7
jotv <josefuccef7@gmail.com>
2026-03-22 14:41:03 +0100

    upd 111

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function subtractOneHour(time) {
   if (ampm === "AM" && hours === 12) hours = 0;
 
   // إنقاص ساعة
-  hours -= 2;
+  hours -= 3;
   if (hours < 0) hours += 12; // ضبط اليوم السابق إذا قلنا عن 0
 
   // إعادة تحويل الوقت إلى صيغة AM/PM إذا كانت موجودة
