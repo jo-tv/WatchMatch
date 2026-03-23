05e0ba308b3742d6a0788934458827e2088a049d
jotv <josefuccef7@gmail.com>
2026-03-23 02:52:38 +0100

    upd 11333

--- index.js
+++ index.js
@@ -404,7 +404,7 @@ const updateMatchStatuses = async () => {
       if (timeDiff > 15 * 60 * 1000) {
         newStatus = "لم تبدأ";
       } 
-      else if ( timeDiff <= 15 * 60 * 1000 && timeDiff >= 0) {
+      else if (timeDiff >= 0 && timeDiff <= 15 * 60 * 1000) {
         newStatus = "ستبدأ بعد قليل"; // ✅ هذا هو الإصلاح الرئيسي
       } 
       else if (timeDiff < 0 && Math.abs(timeDiff) <= 2 * 60 * 60 * 1000) {
